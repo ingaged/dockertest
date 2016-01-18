45bcf57c7fe01424dc64e0859de1e27b3a68fa20
Illya Busigin <illyabusigin@Illyas-MacBook-Pro-2.local>
2016-01-18 11:21:58 -0600

    Tweaking interface to allow specifying credentials and volume path

--- ingaged.go
+++ ingaged.go
@@ -5,15 +5,16 @@ import (
 	"time"
 )
 
-func SetupSFTPContainer() (c ContainerID, ip string, port int, err error) {
+func SetupSFTPContainer(volumePath strings, credentials string) (c ContainerID, ip string, port int, err error) {
 	port = randInt(1024, 49150)
 	forward := fmt.Sprintf("%d:%d", port, 22)
 	if BindDockerToLocalhost != "" {
 		forward = "127.0.0.1:" + forward
 	}
 
+	volume := fmt.Sprintf("%s:/home", volumePath)
 	c, ip, err = setupContainer("atmoz/sftp", port, 10*time.Second, func() (string, error) {
-		res, err := run("--name", "ingaged-sftp-test", "-p", forward, "-d", "atmoz/sftp", "foo:123:1001")
+		res, err := run("--name", "ingaged-sftp-test", "-v", volume, "-p", forward, "-d", "atmoz/sftp", credentials)
 		return res, err
 	})
 	return
